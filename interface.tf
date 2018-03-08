e3fc6563ff09a1a4b01b41eabc9b8d77a0ecdcdc
Umesh <uramesh@amazon.com>
2018-03-07 16:31:08 -0800

    removed CRLF

--- interface.tf
+++ interface.tf
@@ -8,13 +8,11 @@ variable "name" {
      description = "The public subnet to create."
    }
    variable "enable_dns_hostnames" {
-     description = "Should be true if you want to use private DNS
-   within the VPC"
+     description = "Should be true if you want to use private DNS within the VPC"
      default     = true
    }
    variable "enable_dns_support" {
-     description = "Should be true if you want to use private DNS
-   within the VPC"
+     description = "Should be true if you want to use private DNS within the VPC"
      default     = true
 }
 
